aa6a86721cc3ef52d7d77dcf2b91770fdf697348
Anton Krestyaninov <9593795@gmail.com>
2020-06-01 21:15:28 +0300

    Добавлены тесты; улучшены несколько методов

--- classes.rb
+++ classes.rb
@@ -7,11 +7,11 @@ class GraphWindow < Magick::ImageList
 
     settings = read_settings
 
-    self.new_image(settings['image_width'],
-                   settings['image_height'],
-                   Magick::HatchFill.new(settings['grid_main_color'],
-                                         settings['grid_line_color'],
-                                         settings['grid_step']))
+    self.new_image(settings[:image_width],
+                   settings[:image_height],
+                   Magick::HatchFill.new(settings[:grid_main_color],
+                                         settings[:grid_line_color],
+                                         settings[:grid_step]))
 
     GraphImage.take_and_process(settings)
     Candles.new.draw(self)
@@ -28,7 +28,9 @@ class GraphWindow < Magick::ImageList
 
     settings = {}
 
-    doc.each { |i| settings[i[0]] = i[1].match(/\A\d+\z/) ? i[1].to_i : i[1] }
+    doc.each do |i|
+      settings[i[0].to_sym] = i[1].match(/\A\d+\z/) ? i[1].to_i : i[1]
+    end
 
     settings
   end
@@ -40,70 +42,79 @@ class GraphImage < Magick::Draw
     attr_reader :settings
 
     def take_and_process(settings)
-      @settings = add_params(settings)
+      settings[:history]          = candles_unjson
+      settings[:top_extremum]     = top_extremum(settings[:history])
+      settings[:low_extremum]     = low_extremum(settings[:history])
+      settings[:amplitude]        = amplitude(settings)
+      settings[:scale_ratio]      = scale_ratio(settings)
+      settings[:page_bottom]      = page_bottom(settings)
+      settings[:page_top]         = page_top(settings)
+      scale_step_cashe            = scale_step(settings[:amplitude])
+      settings[:scale_main_step]  = scale_step_cashe[0]
+      settings[:scale_small_step] = scale_step_cashe[1]
+      settings[:first_mark]       = find_first_mark(settings)
+
+      @settings = settings
     end
 
     private
 
-    def add_params(settings)
-      settings['history']          = to_points(rate_history)
-      settings['top_extremum']     = top_extremum(settings['history'])
-      settings['low_extremum']     = low_extremum(settings['history'])
-      settings['amplitude']        = amplitude(settings)
-      settings['scale_ratio']      = scale_ratio(settings)
-      settings['page_bottom']      = page_bottom(settings)
-      settings['page_top']         = page_top(settings)
-      scale_step_cashe             = scale_step(settings['amplitude'])
-      settings['scale_main_step']  = scale_step_cashe[0]
-      settings['scale_small_step'] = scale_step_cashe[1]
-      settings['first_mark']       = find_first_mark(settings)
-      settings
+    def candles_unjson
+      history = rate_history
+
+      history.each_key do |key|
+        history[key].transform_keys!(&:to_sym).each_pair do |k, v|
+
+          history[key][k] = currency_rate_to_graph_points(v)
+        end
+      end
+    end
+
+
+    def currency_rate_to_graph_points(value)
+      (value * 10_000).round
     end
 
 
     def rate_history
-      JSON.parse(File.read('data/candles/minute_candles_db.json'))
-          .transform_keys(&:to_i)
+      current_path = File.dirname(__FILE__)
+
+      JSON.parse(File.read(current_path +
+          '/data/candles/minute_candles_db.json')).transform_keys(&:to_i)
     end
 
 
     def top_extremum(history)
-      history.map { |x| x[1]['max'] }.max
+      history.map { |x| x[1][:max] }.max
     end
 
 
     def low_extremum(history)
-      history.map { |x| x[1]['min'] }.min
+      history.map { |x| x[1][:min] }.min
     end
 
 
     def amplitude(settings)
-      settings['top_extremum'] - settings['low_extremum']
+      amplitude = settings[:top_extremum] - settings[:low_extremum]
+      amplitude == 0 ? 1 : amplitude
     end
 
 
     def scale_ratio(settings)
-      (settings['image_height'].to_f - settings['vertical_padding'] * 2) /
-        settings['amplitude']
-    end
-
-
-    def page_bottom(settings)
-      (settings['low_extremum'] - settings['vertical_padding'] /
-        settings['scale_ratio']).ceil
+      (settings[:image_height].to_f - settings[:vertical_padding] * 2) /
+        settings[:amplitude]
     end
 
 
     def page_top(settings)
-      (settings['top_extremum'] + settings['vertical_padding'] /
-        settings['scale_ratio']).floor
+      (settings[:top_extremum] + settings[:vertical_padding] /
+        settings[:scale_ratio]).floor
     end
 
 
-    def find_first_mark(settings)
-      (settings['page_bottom']..).find do |x|
-        (x % settings['scale_main_step']).zero?
-      end
+    def page_bottom(settings)
+      (settings[:low_extremum] - settings[:vertical_padding] /
+        settings[:scale_ratio]).ceil
     end
 
 
@@ -115,52 +126,41 @@ class GraphImage < Magick::Draw
         [2, 1]
       when 13..22
         [5, 1]
-      when 23..45
+      when 23..49
         [10, 5]
-      when 46..90
-        [20, 5]
-      when 90..110
-        [25, 5]
-      when 111..180
-        [40, 10]
-      when 181..270
-        [50, 10]
-      when 271..320
-        [75, 25]
-      when 321...650
-        [100, 50]
       else
-        handsome_round(amplitude)
+        handsome_step(amplitude)
       end
     end
 
 
-    def handsome_round(amplitude)
-      number = amplitude / 5
-      arr    = number.digits.reverse
+    def handsome_step(amplitude)
+      # предварительно-приблизительный шаг основных отметин шкалы
+      approx_step_size = amplitude / 5
 
-      if (3..7).any?(arr[1])
-        arr[1] = 5
+      # попарно: базовые образующие для основных и меньших шагов шкалы
+      base_values = [[10, 2], [20, 5], [25, 5], [50, 10], [100, 20]]
 
-      elsif (0..2).any?(arr[1])
-        arr[1] = 0
+      # разность порядков базовых образующих и приблизительного шага шкалы
+      power_difference = approx_step_size.digits.size - 2
 
-      else
-        arr[0] += 1
-        arr[1]  = 0
+      # приводим базу к порядку приблизительного шага и дополняем пары значением
+      #   удаленности приблизительного шага от каждого комфортного значения
+      upd_values = base_values.map do |arr|
+        arr.map! { |v| v * 10**power_difference }
+        arr << (arr[0] - approx_step_size).abs
       end
 
-      (2...arr.size).each { |i| arr[i] = 0 }
-
-      arr.join.to_i
+      # ищем минимальную удаленность приблизительного шага от комфортного. если
+      #   попались два комфортных значения, до которых расстояние одинаково,
+      #   выбираем больший шаг и возвращаем основной и меньший шаги
+      choice = upd_values.reduce { |c, arr| arr[2] <= c[2] ? arr : c }[0..1]
     end
 
 
-    def to_points(rate_history)
-      rate_history.each_key do |key|
-        rate_history[key].each_pair do |k, v|
-          rate_history[key][k] = (v * 10_000).round
-        end
+    def find_first_mark(settings)
+      (settings[:page_bottom]..).find do |x|
+        (x % settings[:scale_main_step]).zero?
       end
     end
   end
@@ -173,8 +173,8 @@ class GraphImage < Magick::Draw
   private
 
   def to_graph(value, settings)
-    (settings['top_extremum'] - value) *
-      settings['scale_ratio'] + settings['vertical_padding']
+    ((settings[:top_extremum] - value) *
+      settings[:scale_ratio] + settings[:vertical_padding]).to_i
   end
 end
 
@@ -185,9 +185,9 @@ class Candles < GraphImage
 
     settings = GraphImage.settings
 
-    self.stroke(settings['candle_stroke'])
-    self.fill(settings['candle_fill'])
-    self.stroke_width(settings['candle_stroke_width'])
+    self.stroke(settings[:candle_stroke])
+    self.fill(settings[:candle_fill])
+    self.stroke_width(settings[:candle_stroke_width])
 
     draw_candles(settings)
   end
@@ -195,8 +195,8 @@ class Candles < GraphImage
   private
 
   def draw_candles(settings)
-    settings['start_date'].step(settings['finish_date'], 60)
-                          .with_index do |i, nth_candle|
+    settings[:start_date].step(settings[:finish_date], 60)
+                         .with_index do |i, nth_candle|
       candle_cashe = {}
 
       set_candle_opacity(i, settings)
@@ -208,36 +208,36 @@ class Candles < GraphImage
 
 
   def set_candle_opacity(ind, settings)
-    if settings['history'][ind]['start'] < settings['history'][ind]['finish']
-      self.fill_opacity(settings['up_candle_opacity'])
+    if settings[:history][ind][:start] < settings[:history][ind][:finish]
+      self.fill_opacity(settings[:up_candle_opacity])
 
     else
-      self.fill_opacity(settings['down_candle_opacity'])
+      self.fill_opacity(settings[:down_candle_opacity])
     end
   end
 
 
   def draw_candle_body(ind, nth_candle, settings, candle_cashe)
-    candle_cashe['start']  = to_graph(settings['history'][ind]['start'],
-                                      settings)
-    candle_cashe['finish'] = to_graph(settings['history'][ind]['finish'],
-                                      settings)
+    candle_cashe[:start]  = to_graph(settings[:history][ind][:start],
+                                     settings)
+    candle_cashe[:finish] = to_graph(settings[:history][ind][:finish],
+                                     settings)
 
-    self.rectangle(nth_candle * settings['density'],
-                   candle_cashe['start'],
+    self.rectangle(nth_candle * settings[:density],
+                   candle_cashe[:start],
 
-                   nth_candle * settings['density'] + settings['thickness'],
-                   candle_cashe['finish'] + 1)
+                   nth_candle * settings[:density] + settings[:thickness],
+                   candle_cashe[:finish] + 1)
   end
 
 
   def draw_candle_shadows(ind, nth_candle, settings, candle_cashe)
-    min = to_graph(settings['history'][ind]['min'], settings)
-    max = to_graph(settings['history'][ind]['max'], settings)
+    min = to_graph(settings[:history][ind][:min], settings)
+    max = to_graph(settings[:history][ind][:max], settings)
 
-    high_end = [candle_cashe['start'], candle_cashe['finish']].min
-    low_end  = [candle_cashe['start'], candle_cashe['finish']].max
-    middle   = nth_candle * settings['density'] + settings['thickness'] / 2
+    high_end = [candle_cashe[:start], candle_cashe[:finish]].min
+    low_end  = [candle_cashe[:start], candle_cashe[:finish]].max
+    middle   = nth_candle * settings[:density] + settings[:thickness] / 2
 
     self.line(middle, high_end, middle, max) if max != high_end
 
@@ -252,16 +252,16 @@ class LeftScale < GraphImage
 
     settings = GraphImage.settings
 
-    self.stroke(settings['scale_stroke'])
-    self.stroke_opacity(settings['scale_stroke_opacity'])
-    self.pointsize(settings['font_size'])
+    self.stroke(settings[:scale_stroke])
+    self.stroke_opacity(settings[:scale_stroke_opacity])
+    self.pointsize(settings[:font_size])
     self.text_undercolor('#FFFFFFA5')
 
-    self.line(settings['scale_margin'],
+    self.line(settings[:scale_margin],
               0,
 
-              settings['scale_margin'],
-              settings['image_height'])
+              settings[:scale_margin],
+              settings[:image_height])
 
     draw_main_marks(settings)
     draw_small_marks(settings)
@@ -270,41 +270,48 @@ class LeftScale < GraphImage
   private
 
   def draw_main_marks(settings)
-    settings['first_mark'].step(settings['page_top'],
-                                settings['scale_main_step']) do |mark|
+    settings[:first_mark].step(settings[:page_top],
+                               settings[:scale_main_step]) do |mark|
       y_coord_cashe = to_graph(mark, settings)
 
-      self.line(settings['scale_margin'],
+      self.line(settings[:scale_margin],
                 y_coord_cashe,
 
-                settings['scale_margin'] + settings['scale_mark_size'],
+                settings[:scale_margin] + settings[:scale_mark_size],
                 y_coord_cashe)
 
-      self.text(settings['scale_margin'] + settings['text_left_padding'],
-                y_coord_cashe - settings['text_vert_padding'],
-                mark.to_s.insert(1, '.'))
+      self.text(settings[:scale_margin] + settings[:text_left_padding],
+                y_coord_cashe - settings[:text_vert_padding],
+                mark_value(mark))
     end
+  end
+
+
+  def mark_value(mark)
+    mark = mark.to_s
+    (5 - mark.size).times { mark.prepend('0') } if mark.size < 5
+    mark.insert(-5, '.')
   end
 
 
   def draw_small_marks(settings)
 
     # сокращение шапок итераторов
-    first_mark = settings['first_mark']
-    top        = settings['page_top']
-    bottom     = settings['page_bottom']
-    step       = settings['scale_small_step']
+    first_mark = settings[:first_mark]
+    top        = settings[:page_top]
+    bottom     = settings[:page_bottom]
+    step       = settings[:scale_small_step]
 
 
     # отрисовка засечек вверх от первой (first_mark)
     first_mark.step(top, step) do |mark|
-      if mark % settings['scale_main_step']
+      if mark % settings[:scale_main_step]
         y_coord_cashe = to_graph(mark, settings)
 
-        self.line(settings['scale_margin'],
+        self.line(settings[:scale_margin],
                   y_coord_cashe,
 
-                  settings['scale_margin'] + settings['scale_mark_size'] / 2,
+                  settings[:scale_margin] + settings[:scale_mark_size] / 2,
                   y_coord_cashe)
       end
     end
@@ -314,10 +321,10 @@ class LeftScale < GraphImage
     (first_mark - step).step(bottom, - step) do |mark|
       y_coord_cashe = to_graph(mark, settings)
 
-      self.line(settings['scale_margin'],
+      self.line(settings[:scale_margin],
                 y_coord_cashe,
 
-                settings['scale_margin'] + settings['scale_mark_size'] / 2,
+                settings[:scale_margin] + settings[:scale_mark_size] / 2,
                 y_coord_cashe)
     end
   end
---
+++ spec/graph_window_spec.rb
@@ -0,0 +1,225 @@
+require 'rspec'
+require 'rmagick'
+require 'json'
+require_relative '../classes.rb'
+
+
+describe 'GraphWindow' do
+  before(:all) do
+    graph             = GraphWindow.new
+    @default_settings = graph.send(:read_settings)
+    @settings         = GraphImage.take_and_process(graph.send(:read_settings))
+  end
+
+
+  describe 'read_settings' do
+    it 'should return a hash' do
+      expect(@default_settings).to be_a Hash
+    end
+
+
+    it 'should return a hash with precisely 22 pairs' do
+      expect(@default_settings.size).to eq 22
+    end
+
+
+    it 'sould return a hash with precisely 5 string values' do
+      expect(@default_settings[:grid_main_color]).to be_a String
+      expect(@default_settings[:grid_line_color]).to be_a String
+      expect(@default_settings[:candle_stroke])  .to be_a String
+      expect(@default_settings[:candle_fill])    .to be_a String
+      expect(@default_settings[:scale_stroke])   .to be_a String
+    end
+
+
+    it 'sould return a hash with precisely 17 integer values' do
+      expect(@default_settings.values.count { |v| v.class == Integer }).to eq 17
+    end
+
+
+    it 'should return a hash with only symbols as keys' do
+      @default_settings.each_key { |k| expect(k).to be_a Symbol }
+    end
+  end
+
+
+  describe 'take and process' do
+    it 'should pass to GraphImage class a hash with 32 pairs' do
+      expect(GraphImage.settings.size).to eq 32
+    end
+  end
+
+
+  describe 'rate_history' do
+    before(:all) do
+      @history = GraphImage.send(:rate_history)
+    end
+
+
+    it 'should return a hash with only integers as keys' do
+      @history.each_key { |k| expect(k).to be_a Integer }
+    end
+
+
+    it 'should return a hash with only hashes as values' do
+      @history.each_value { |v| expect(v).to be_a Hash }
+    end
+
+
+    it 'should return a hash with values sizes precisely 4' do
+      @history.each_value { |v| expect(v.size).to eq 4 }
+    end
+
+
+    it 'should return a hash with only floats as subhashes values' do
+      @history.each_key do |k|
+        @history[k].each_value { |v| expect(v).to be_a Float }
+      end
+    end
+
+
+    it 'should return a hash with only values greater than 0' +
+       'in subhashes values' do
+
+      @history.each_key do |k|
+        @history[k].each_value { |v| expect(v).to be > 0 }
+      end
+    end
+
+
+    it 'should return a hash with no number bigger than max' +
+       'in subhashes values' do
+
+      @history.each_key do |k|
+        @history[k].each_value { |v| expect(v).to be <= @history[k]['max'] }
+      end
+    end
+
+
+    it 'should return a hash with no number less than min' +
+       'in subhashes values' do
+
+      @history.each_key do |k|
+        @history[k].each_value { |v| expect(v).to be >= @history[k]['min'] }
+      end
+    end
+
+
+    it 'should return a hash with exactly start, finish, min, max strins' +
+       'as keys in subhashes values' do
+
+      @history.each_value do |v|
+        expect(v.keys.sort).to eq ['finish', 'max', 'min', 'start']
+      end
+    end
+  end
+
+
+  describe 'amplitude' do
+    it 'should always return positive number' do
+      expect(GraphImage.send(:amplitude, @settings)).to be > 0
+    end
+  end
+
+
+  describe 'scale_ratio' do
+    it 'should return a value greater than 0' do
+      expect(GraphImage.send(:scale_ratio, @settings)).to be > 0
+    end
+  end
+
+
+  describe 'page_bottom' do
+    before(:all) do
+      rate_value = GraphImage.send(:page_bottom, @settings)
+      @y_coord   = GraphImage.new.send(:to_graph, rate_value, @settings)
+    end
+
+
+    it "should never return values that render lower than" +
+       "the image's bottom edge" do
+
+      expect(@y_coord).not_to be > @settings[:image_height] - 1
+    end
+
+
+    it "should never return values that render higher than" +
+       "the image's bottom edge more than by scale ratio" do
+
+      expect(@y_coord).not_to be <
+        @settings[:image_height] - 1 - @settings[:scale_ratio]
+    end
+  end
+
+
+  describe 'page_top' do
+    before(:all) do
+      rate_value = GraphImage.send(:page_top, @settings)
+      @y_coord   = GraphImage.new.send(:to_graph, rate_value, @settings)
+    end
+
+
+    it "should never return values that render higher than" +
+       "the image's top edge" do
+
+      expect(@y_coord).not_to be < 0
+    end
+
+
+    it "should never return values that render lower than" +
+       "the image's top edge more than by scale_ratio" do
+
+      expect(@y_coord).not_to be > @settings[:scale_ratio]
+    end
+  end
+
+
+  describe 'find_first_mark' do
+    it "should never return values that render lower than" +
+       "the image's bottom edge more than by scale_main step" do
+
+      rate_value = GraphImage.send(:find_first_mark, @settings)
+      y_coord    = GraphImage.new.send(:to_graph, rate_value, @settings)
+
+      expect(y_coord).not_to be < @settings[:image_height] - 1 -
+        @settings[:scale_main_step] * @settings[:scale_ratio]
+    end
+  end
+
+
+  describe 'candles_unjson' do
+    before(:all) do
+      @history = GraphImage.send(:candles_unjson)
+    end
+
+
+    it 'should only return a hash with symblos as subhashes keys' do
+      @history.each_value do |v|
+        v.each_key { |k| expect(k).to be_a Symbol }
+      end
+    end
+
+
+    it 'should only return a hash with integers as subhashes values' do
+      @history.each_value do |val|
+        val.each_value { |val| expect(val).to be_an Integer }
+      end
+    end
+  end
+
+
+  describe 'to_graph' do
+    it 'should always return values between image top and bottom edges' do
+      upper_edge  = @settings[:top_extremum]
+      lower_edge  = @settings[:low_extremum]
+      graph_image = GraphImage.new
+
+      1000.times do
+        coord = graph_image.send(:to_graph,
+                                 rand(lower_edge..upper_edge),
+                                 @settings)
+        expect(coord).to be_between(0, 719)
+      end
+    end
+  end
+end
